5b00da3eec5b03a881fce51d19d08b87eb25c3b5
Zsxzexploitsrblx <johannesfrroose@gmail.com>
2025-05-14 17:40:00 +0300

    Update brick.lua

--- brick.lua
+++ brick.lua
@@ -1,1 +1,9 @@
---
+-- create a brick by typing
+local newBrick = Instance.new("Part", workplace)
+-- change the color by tping this
+newBrick.BrickColor = "Really Red" -- any color you want here
+-- change the transparency by typing someting like this
+newBrick.Transparency = 0.5 -- whatever transparency you want
+-- if you want to make go in the sky and drop down type this
+newBrick.Position = Vector3.new(0, 30, 0) -- the first is x second is y (the part that decides wheter or not its 50 units up in the air or 50 units down and lastly the third is z)
+-- now you're done :]
